007014a86757fe197678a461959f856fa72c3ca9
VisualSource <collin_blosser@yahoo.com>
2024-04-24 23:24:04 -0400

    compiler

---
+++ cpp/include/vip/compiler/targets/Chip8Object.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "../Object.hpp"
+
+namespace compiler
+{
+    class Chip8Object : public Object
+    {
+    private:
+    public:
+        Chip8Object() : Object(0) {}
+    };
+} // namespace compiler
